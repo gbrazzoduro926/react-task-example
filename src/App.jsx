de77f26cdf6649f8c005d26ca3234d531a135749
Gbrazz17 <gabrielbrazzoduro@gmail.com>
2022-10-03 11:27:31 -0400

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import TaskList from "./components/TaskList";
 import TaskForm from "./components/TaskForm";
 import "./css/Task.css";
+import { useContext } from "react";
+import { TaskContext } from "./context/TaskContext";
+import { isEmpty } from "lodash";
 
 function App() {
+  const { tasks } = useContext(TaskContext);
   return (
     <main className="main">
       <div>
         <TaskForm />
-        <div className="container1">
+        <div className={isEmpty(tasks) ? "container" : "container1"}>
           <TaskList />
         </div>
       </div>
